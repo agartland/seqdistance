Fixed normalization with nans/gaps.

nbmetrics.py
@@ -45,9 +45,12 @@ def nb_seq_similarity(seq1, seq2, substMat, normed = True, asDistance = False):
     """Computes sequence similarity based on the substitution matrix."""
     assert seq1.shape[0] == seq2.shape[0]
 
+    site12N = 0.
     if normed:
+        site11N = 0.
+        site22N = 0.
+
         sim12 = 0.
-        site12N = 0.
         sim11 = 0.
         sim22 = 0.
         for i in range(seq1.shape[0]):
@@ -59,23 +62,32 @@ def nb_seq_similarity(seq1, seq2, substMat, normed = True, asDistance = False):
                 site12N += 1.
             if not np.isnan(cur11):
                 sim11 += cur11
+                site11N += 1.
             if not np.isnan(cur22):
                 sim22 += cur22
-        sim12 = 2*sim12/((sim11/site12N) + (sim22/site12N))
+                site22N += 1.
+        if site11N == 0 or site22N == 0:
+            sim12 = np.nan
+        else:
+            sim12 = 2*sim12/((sim11/site11N) + (sim22/site22N))
     else:
         sim12 = 0.
-        site12N = 0.
         for i in range(seq1.shape[0]):
-            if not np.isnan(substMat[seq1[i],seq2[i]]):
-                sim12 += substMat[seq1[i],seq2[i]]
+            cur12 = substMat[seq1[i],seq2[i]]
+            if not np.isnan(cur12):
+                sim12 += cur12
                 site12N += 1.
 
     if asDistance:
         if normed:
-            sim12 = (site12N - sim12)/site12N
+            if site12N == 0.:
+                sim12 = np.nan
+            else:
+                sim12 = (site12N - sim12)/site12N
         else:
             sim12 = site12N - sim12
     return sim12
+
 @nb.jit(nb.float64(nb.int8[:],nb.int8[:],nb.float64[:,:],nb.boolean), nopython = True)
 def nb_seq_distance(seq1, seq2, substMat, normed = False):
     """Compare two sequences and return the distance from one to the other

npmetrics.py
@@ -41,9 +41,13 @@ def np_seq_similarity(seq1, seq2, substMat, normed = True, asDistance = False):
         siteN = (~np.isnan(sim12)).sum()
 
     if normed:
-        sim11 = np.nansum(substMat[seq1,seq1])/siteN
-        sim22 = np.nansum(substMat[seq2,seq2])/siteN
-        tot12 = 2*np.nansum(sim12)/(sim11+sim22)
+        sim11 = substMat[seq1,seq1]
+        sim22 = substMat[seq2,seq2]
+
+        site11N = (~np.isnan(sim11)).sum()
+        site22N = (~np.isnan(sim22)).sum()
+
+        tot12 = 2*np.nansum(sim12)/(np.nansum(sim11)/site11N + np.nansum(sim22)/site22N)
     else:
         tot12 = np.nansum(sim12)
 

strmetrics.py
@@ -50,9 +50,7 @@ def str_seq_similarity(seq1, seq2, subst = None, normed = True, asDistance = Fal
         sim12 = nansum(2*sim12/(nanmean(sim11) + nanmean(sim22))
     Optionally specify normed = False:
         [0, total raw similarity]
-    This returns a score [0, 1] for binary and blosum based similarities
-        otherwise its just the sum of the raw score out of the subst matrix
-    
+        
     Optionally return a distance instead of a similarity.
     Distance between seq1 and seq2 using the substitution matrix subst
     because seq_similarity returns a total similarity with max of siteN (not per site), we use
@@ -78,9 +76,14 @@ def str_seq_similarity(seq1, seq2, subst = None, normed = True, asDistance = Fal
         siteN = np.sum(~np.isnan(sim12))
 
     if normed:
-        sim11 = str_seq_similarity(seq1, seq1, subst=subst, normed=False) / siteN
-        sim22 = str_seq_similarity(seq2, seq2, subst=subst, normed=False) / siteN
-        tot12 = np.nansum(2*sim12/(sim11+sim22))
+        sim11 = np.array([i for i in itertools.imap(lambda a,b: subst.get((a,b),subst.get((b,a),np.nan)), seq1, seq1)])
+        sim22 = np.array([i for i in itertools.imap(lambda a,b: subst.get((a,b),subst.get((b,a),np.nan)), seq2, seq2)])
+        site11N = np.sum(~np.isnan(sim11))
+        site22N = np.sum(~np.isnan(sim22))
+        if site11N == 0 or site22N == 0:
+            tot12 = np.nan
+        else:
+            tot12 = np.nansum(2*sim12/(np.nansum(sim11)/site11N + np.nansum(sim22)/site22N))
     else:
         tot12 = np.nansum(sim12)
 

test_all.py
@@ -58,16 +58,17 @@ similarity_equals = [ [('AAAA','AAAA', s, False), 4],
                        [('AAAA-','-AAAA', sGap, True), 3],
                        [('-AAA-','-AAAA', sGap, False), 4],
                        [('-AAA-','-AAAA', sGap, True), 4],
-                       [('AAAAK','-AAAK', nanS, True), 3.55555],
-                       [('AAAA-','-AAAA', nanS, True), 2.25],
-                       [('AAAAK','-AAAK', nanS, False), 4],
+                       [('AAAAK','-AAAK', nanS, True), 4.0],
+                       [('AAAA-','-AAAA', nanS, True), 3.0],
+                       [('AAAAK','-AAAK', nanS, False), 4.0],
                        [('AAAA-','-AAAA', nanS, False), 3],
-                       [('AIAAK','-AAAK', nanS, True), 2.66666],
+                       [('AIAAK','-AAAK', nanS, True), 3],
                        [('AIAAK','-AAAK', nanS, False), 3],
                        [('AAAA-','-AAAA', nanS, False), 3],
-                       [('AAAA-','-AAAA', nanS, True), 2.25],
+                       [('AAAA-','-AAAA', nanS, True), 3],
                        [('-AAA-','-AAAA', nanS, False), 3],
-                       [('-AAAI-','-AAAAK', nanS, True), 2.666666] ]
+                       [('-AAAI-','-AAAAK', nanS, True), 3],
+                       [('AAA----','---KKKK',s,True), 0] ]
 
 distance_equals = [ [('AAAA','AAAA', s, True), 0],
                    [('AAAA','AAAA', s, False), 0],
@@ -85,12 +86,12 @@ distance_equals = [ [('AAAA','AAAA', s, True), 0],
                    [('AAAAK','-AAAK', nanS, False), 0],
                    [('AAAA-','-AAAA', nanS, False), 0],
                    [('AIAAK','-AAAK', nanS, False), 1],
-                   [('-AAA-','-AAAA', nanS, False), 0],
-                   [('AAAAK','-AAAK', nanS, True), 0.11111],
-                   [('AAAA-','-AAAA', nanS, True), 0.25],
-                   [('AIAAK','-AAAK', nanS, True), 0.33333],
-                   [('-AAAI-','-AAAAK', nanS, True), 0.33333],
-                   [('AAAA-','-AAAA', nanS, True), 0.25],
+                   [('-AAA-','-AAAA', nanS, False), 0.0],
+                   [('AAAAK','-AAAK', nanS, True), 0.0],
+                   [('AAAA-','-AAAA', nanS, True), 0.0],
+                   [('AIAAK','-AAAK', nanS, True), 0.25],
+                   [('-AAAI-','-AAAAK', nanS, True), 0.25],
+                   [('AAAA-','-AAAA', nanS, True), 0.0],
                    [('AAAA-','-AAAA', nanS, False), 0.0] ]
 
 class TestTools(unittest.TestCase):
@@ -138,10 +139,12 @@ class TestStrMetrics(unittest.TestCase):
         """str_seq_similarity(seq1, seq2, subst = None, normed = True, asDistance = False)
         asDistance = True is tested in seq_distance"""
         for i, (args, res) in enumerate(similarity_equals):
-            self.assertAlmostEqual(strmetrics.str_seq_similarity(*args), res, places = 3, msg = "Test %d: (%s, %s)" % (i,args[0],args[1]))
+            self.assertAlmostEqual(strmetrics.str_seq_similarity(*args), res, places = 3, msg = "Test %d: (%s, %s) not %1.2f?" % (i,args[0],args[1],strmetrics.str_seq_similarity(*args)))
 
         with self.assertRaises(AssertionError):
             strmetrics.str_seq_similarity('AAAA','AA')
+
+        self.assertTrue(np.isnan(strmetrics.str_seq_similarity('-------','---KKKK',s,True,False)))
 
     def test_seq_distance(self):
         """str_seq_distance(seq1, seq2, subst = None, normed = True)"""
@@ -196,6 +199,7 @@ class TestNpMetrics(unittest.TestCase):
                 sMat = matrices.subst2mat(args[2])
             self.assertEqual(self.seq_similarity(seq2vec(args[0]),seq2vec(args[1]),sMat,args[3],False),
                              strmetrics.str_seq_similarity(*args), msg = "Test %d: (%s, %s)" % (i,args[0],args[1]))
+        self.assertTrue(np.isnan(self.seq_similarity(seq2vec('-------'),seq2vec('---KKKK'),sMat,True,False)))
     def test_coverage_distance(self):
         """np_coverage_distance(seqVec1, seqVec2, mmTolerance))"""
         for i, (args, res) in enumerate(coverage_equals):
@@ -236,7 +240,7 @@ if NB_SUCCESS:
 
 class TestDistRect(unittest.TestCase):
     def setUp(self):
-        self.seq = tools.removeBadAA("MGARASVLSGGELDRWEKIRLRPGGKKKYKLKHIVWASRELERFAVNPGL")
+        self.seq = tools.removeBadAA("MGARA-SVLSGGELDRW-EKIRLRP-GGKKKYKLKHIVW-ASRELERFAVNPGL")
         self.mers = [self.seq[starti:starti + 9] for starti in range(len(self.seq)-9+1)]
         self.mers15 = [self.seq[starti:starti + 15] for starti in range(len(self.seq)-15+1)]
     def test_numpy(self):